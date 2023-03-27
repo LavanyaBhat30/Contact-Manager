19859422e9699a371ebdc0c33323dbedc1ba2f02
M.Divyasree <divyasreemannava@gmail.com>
2023-03-27 21:05:58 +0530

    adding frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
-import Login from "./components/login";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Signup from "./components/signUp";
-import React from 'react'
-import Logout from "./components/contacts";
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>}></Route>
-        <Route path="/register" element={<Signup/>}></Route>
-        <Route path="/contacts" element={<Logout/>}></Route>
-
-      </Routes>
-    </BrowserRouter>
-  );
+import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom' 
+import Login from './components/log/Login';
+import Signup from './components/log/Signup';
+import Main from './components/Main/Main'
+// import ImportFile from './components/ImportFile';
+import ProtectedRoutes from './components/protectedRoutes';
+const App=()=> {
+ return(
+  <>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Login/>} />
+    <Route path='/signup' element={<Signup/>}/>
+    <Route element={<ProtectedRoutes/>}>
+    <Route path='/main' element={<Main/>}/>
+    </Route>
+  </Routes>
+  </BrowserRouter>
+  </>
+ )
 }
 
 export default App;
