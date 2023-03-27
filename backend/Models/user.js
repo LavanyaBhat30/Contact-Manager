0d41ef4561f9b398b7629ad08c152eae32ced8dd
Narmadha M <79155752+narmadh@users.noreply.github.com>
2023-03-27 22:50:39 +0530

    made changes to delete functionality

--- backend/Models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-
-const userSchema =new mongoose.Schema({
-    email: {type: String, unique:true}, 
-    password: String
-  },{timestamps:true});
-  const users = mongoose.model("users", userSchema, "users");
-
-  module.exports= users
